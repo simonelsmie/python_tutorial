a7aac34201705c01ccca7e3cd8bb3eb4f8685e9b
Simon Elsmie <simon.elsmie@esure.com>
2022-06-06 15:08:01 +0100

    Completed loops

--- 5_loops.py
+++ 5_loops.py
@@ -34,10 +34,19 @@ while (counter < 5):
 
 # For loops only iterate for the length of the list
 numbers  = [1, 2, 3, 4, 5]
-for number in numbers:
-    print(number * number)
+# for number in numbers:
+#     print(number * number)
     
 
 # How would we total all the numbers in numbers?
 
 
+employees = [
+    {"name": "Chuck", "age": 40, "service_years": 3},
+    {"name": "Tom", "age": 60, "service_years": 15},
+    {"name": "Sean", "age": 25, "service_years": 1},
+    {"name": "Viola", "age": 36, "service_years": 5},
+    {"name": "Serena", "age": 53, "service_years": 20},
+]
+
+# How can we find the total years of service for all employees?
