95d784d0bb1138fb79990b556da03d7d3817ef74
Simon Elsmie <simon.elsmie@esure.com>
2022-06-16 16:24:30 +0100

    with examples from tutorial

--- 2.2_practice.py
+++ 2.2_practice.py
@@ -1,25 +1,39 @@
 # Create a function that takes in an argument of fruit, so that it will print "You're favourite fruit is " followed by the fruit provided.
 # Make sure to call this function a few times with different parameters given so that we see the string printed with different fruit.
 # >> Define function here
+from audioop import add
 
 
+def fruit_picker(fruit):
+    print("Your favourite fruit is " + fruit)
+
+fruit_picker("Apples")
 
 
 # Create a function that can add two integer values together that are provided as arguments.
 # Print this value so that we can see the total of the two integers added together.
 # >> Define function here
+def addition(num1, num2):
+    print(num1 + num2)
 
+addition(5, 2)
+addition("5", "2") 
+# Strings so concatinated
 
 
 # Create a function that can take an argument of name and of time of day, so that the printed value could show "Good morning Dan" for example
 # >> Define function here
+def greeting(name, time):
+    print("Good " + time + ", " + name)
+
+greeting("Dan", "morning")
 
 
 
 # Please feel free to create other functions, including other math operators.
 # If you would like a challenge then you could use the in-build function of input() to get a bit more interaction as shown below:
 def test_name():
-    name = input("What is your name? ")
+    name = input("What is your name? \n")
     print("This is just a test, but hello " + name)
 
 test_name()
--- 3_lists.py
+++ 3_lists.py
@@ -8,38 +8,61 @@
 fruit = ["apple", "banana", "kiwi", "pear", "grape"]
 
 # Print the list
-
+# print(fruit)
 
 # Indexes - what are they and where do they start from
 
 
 # Print off pear by the index of fruit
-
+# print(fruit[3])
+# print(fruit[0])
+# print(fruit[1])
 
 # Indexing from the end of the list
+# print(fruit[-1])
 
+# Length of a list
+# print(len(fruit))
 
 # Update an entry in the list
 # Change Banana to Mango
+fruit[1] = "mango"
+# print(fruit)
 
 
 # Create an empty list
-
+new_list = []
+new_list2 = list()
 
 # List functions
 # Adding to a list - using append
+# print(new_list)
+new_list.append("Adela")
+new_list.append("Matt")
+new_list.append("Mike")
+# print(new_list)
 
 
 # Removing from the end of a list - using pop
+# new_list.pop()
+# print(new_list)
 
+# new_list.pop(1)
+# Giving arguement replaces default of last place to that index value
 
 ## You can remove items by their value - using remove
-
+fruit.remove("kiwi")
+print(fruit)
 
 # Adding/combining lists
-
+combined_list = fruit + new_list
+print(combined_list)
 
 # Extending Lists
+print(new_list)
+new_list.extend(fruit)
+print(new_list)
 
 
 # Nested lists
+nested = [1, 3, 4, [5, 6, 7]]
